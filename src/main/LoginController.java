e0863fffe0d75d37b38ca0566ca193edafa10f56
Asif Mahmood <asif.mahmood@iub.edu.bd>
2024-02-05 10:11:35 +0600

    Updated text of the sign in Button.

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -26,7 +26,7 @@ public class LoginController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        label.setText("Please enter a valid username and password");
     }
     
     @Override
